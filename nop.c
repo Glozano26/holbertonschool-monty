ea46850926607b45f2ab5336c0491c279498ad9e
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-07 15:41:43 -0700

    nop

--- nop.c
+++ nop.c
@@ -5,9 +5,8 @@
  *@line_number: line_number
  *Return: no return
  */
-int nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
-	return (0);
 }
