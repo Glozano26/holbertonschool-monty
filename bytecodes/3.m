dbe701054f9905aa6dc1ef28ac0e53d5321c8f1d
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-08 09:17:17 -0700

    swap

---
+++ bytecodes/3.m
@@ -0,0 +1,5 @@
+push 98
+push 402
+push 15
+swap Holberton
+pall
--- swap.c
+++ swap.c
@@ -9,13 +9,19 @@
 
 void swap(stack_t **stack, unsigned int line_number)
 {
+	int temp2;
         stack_t *node_to_swap = *stack;
         (void)line_number;/*Parametro no Utilizado*/
 
-        *stack = node_to_swap->next;
+        /* *stack = node_to_swap->next;
         (*stack)->prev = NULL;
         node_to_swap->prev = (*stack)->next->prev;
         (*stack)->next->prev = node_to_swap;
         node_to_swap->next = (*stack)->next;
-        (*stack)->next = node_to_swap;
+        (*stack)->next = node_to_swap; */
+
+	node_to_swap = *stack;
+	temp2 = node_to_swap->n;
+	node_to_swap->n = node_to_swap->next->n;
+	node_to_swap->next->n = temp2;
 }
