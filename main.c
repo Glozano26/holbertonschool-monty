48b1a6276f3dbc3d16548352a5e7e8e04f4fa237
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-09 13:55:53 -0700

    comentando la funcion delete a ver si funciona el programa2

--- main.c
+++ main.c
@@ -172,48 +172,6 @@ int sum_dlistint(stack_t *stack)
 }
 
 /**
- * delete_dnodeint_at_index - func that delet a new node at a given position
- * @stack: pointer to the header of the nodes
- * @index: the index of the list where the new node should be added
- * Return: 1 if it succeeded, -1 if it failed
- */
-/*
-int delete_dnodeint_at_index(stack_t **stack, unsigned int index)
-{
-	unsigned int i;
-	stack_t *node_to_delete;
-
-	if (!stack || !*stack)
-		return (-1);
-	node_to_delete = *stack;
-
-	if (index == 0)
-	{
-		*stack = node_to_delete->next;
-		if (node_to_delete->next)
-			node_to_delete->next->prev = NULL;
-		free(node_to_delete);
-		return (1);
-	}
-	for (i = 0; node_to_delete && i < index; i++)
-	{
-		node_to_delete = node_to_delete->next;
-	}
-	if (!node_to_delete)
-		return (-1);
-	if (!node_to_delete->next)
-	{
-		node_to_delete->prev->next = NULL;
-		free(node_to_delete);
-		return (1);
-	}
-	node_to_delete->prev->next = node_to_delete->next;
-	node_to_delete->next->prev = node_to_delete->prev;
-	free(node_to_delete);
-	return (1);
-}
-*/
-/**
  *  invalid_instruct- Handles the case of an unknown instruction
  * @line_number: The line number in the script
  * @opcode: The unknown instruction
